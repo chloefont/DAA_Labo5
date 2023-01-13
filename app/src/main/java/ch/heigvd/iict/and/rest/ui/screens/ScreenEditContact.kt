830a5eee1bb1ba42f68409ebb12b96302e5cfd20
chloe <cfontaine@citycable.ch>
2023-01-13 17:58:36 +0100

    WIP

--- app/src/main/java/ch/heigvd/iict/and/rest/ui/screens/ScreenEditContact.kt
+++ app/src/main/java/ch/heigvd/iict/and/rest/ui/screens/ScreenEditContact.kt
@@ -1,9 +1,15 @@
 package ch.heigvd.iict.and.rest.ui.screens
 
 import android.annotation.SuppressLint
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.Scaffold
 import androidx.compose.material.Text
+import androidx.compose.material.TextField
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
 import androidx.navigation.NavHostController
 import ch.heigvd.iict.and.rest.models.Contact
 import ch.heigvd.iict.and.rest.navigation.AppScreens
@@ -17,6 +23,21 @@ fun ScreenEditContact(navController : NavHostController, contact : Long?) {
             canNavigateBack = true,
             onNavigateBack = { navController.navigateUp() }) }
     ) {
-        Text(text = "new contact")
+        Column() {
+            Text(text = "new contact")
+            nameTextFieldItem(name = "Cux", placeHolder = "Name", null)
+        }
+    }
+}
+
+@Composable
+fun nameTextFieldItem(name : String, placeHolder : String, value : String?) {
+    Row(modifier = Modifier.fillMaxSize(), horizontalArrangement = Arrangement.SpaceBetween) {
+        Text(text = name)
+        if (value != null) {
+            TextField(value = "", onValueChange = {}, placeholder = { Text(text = placeHolder!!) })
+        } else {
+            TextField(value = "", placeholder = { Text(text = placeHolder!!) }, onValueChange = {})
+        }
     }
 }
