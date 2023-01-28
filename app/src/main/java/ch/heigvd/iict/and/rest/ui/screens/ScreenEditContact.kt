22f1d53d3d16f289b8c2b011c87835c22b9912ce
chloe <cfontaine@citycable.ch>
2023-01-28 13:25:39 +0100

    form ok

--- app/src/main/java/ch/heigvd/iict/and/rest/ui/screens/ScreenEditContact.kt
+++ app/src/main/java/ch/heigvd/iict/and/rest/ui/screens/ScreenEditContact.kt
@@ -1,12 +1,9 @@
 package ch.heigvd.iict.and.rest.ui.screens
 
 import android.annotation.SuppressLint
+import android.icu.text.SimpleDateFormat
 import android.util.Log
 import androidx.compose.foundation.layout.*
-import androidx.compose.material.RadioButton
-import androidx.compose.material.Scaffold
-import androidx.compose.material.Text
-import androidx.compose.material.TextField
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.*
@@ -25,7 +22,7 @@ import ch.heigvd.iict.and.rest.models.PhoneType
 import ch.heigvd.iict.and.rest.navigation.AppScreens
 import ch.heigvd.iict.and.rest.ui.TopBar
 import ch.heigvd.iict.and.rest.viewmodels.ContactsViewModel
-import java.util.Calendar
+import java.util.*
 
 @SuppressLint("UnusedMaterialScaffoldPaddingParameter")
 @Composable
@@ -51,87 +48,74 @@ fun ScreenEditContact(navController: NavHostController, contact: Long?, contacts
         }
     }
 
-
-
     val (selected, setSelected) = remember { mutableStateOf("")}
+    val simpleDateFormat = SimpleDateFormat("dd-MM-yyyy")
 
-        val (nameValue, setNameValue) = remember {
-            mutableStateOf(currentContact.name)
+    Scaffold(
+        topBar = {
+            TopBar(currentScreen = AppScreens.EditContact,
+                canNavigateBack = true,
+                onNavigateBack = { navController.navigateUp() })
         }
-        val firstnameValue = currentContact?.firstname
-        val emailValue = currentContact?.email
-        val birthdayValue = currentContact?.birthday ?: Calendar.getInstance()
-        val addressValue = currentContact?.address
-        val zipValue = currentContact?.zip
-        val cityValue = currentContact?.city
-        val phonetypeValue = currentContact?.type ?: PhoneType.MOBILE
-        val phonenumberValue = currentContact?.phoneNumber
-
-        Scaffold(
-            topBar = {
-                TopBar(currentScreen = AppScreens.EditContact,
-                    canNavigateBack = true,
-                    onNavigateBack = { navController.navigateUp() })
-            }
+    ) {
+        Column(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(10.dp, 0.dp)
+                .verticalScroll(rememberScrollState()), verticalArrangement = Arrangement.spacedBy(10.dp)
         ) {
-            Column(
-                modifier = Modifier
-                    .fillMaxSize()
-                    .padding(10.dp, 0.dp)
-                    .verticalScroll(rememberScrollState()), verticalArrangement = Arrangement.spacedBy(10.dp)
-            ) {
-                Text(text = "New contact")
-                textFieldItem(name = "Name", placeHolder = "Name", value = currentContact.name, onValueChanged = {
-                    Log.d("ScreenEditContact", "Name changed to $it")
-                    setCurrentContact(currentContact.copy(name = it!!))
-                })
-                textFieldItem(name = "Firstname", placeHolder = "Firstname", firstnameValue, onValueChanged = { currentContact.firstname = it })
-                textFieldItem(name = "Email", placeHolder = "Email", emailValue, onValueChanged = { currentContact.email = it })
-                textFieldItem(name = "Birthday", placeHolder = "Birthday", "birthdayValue")
-                textFieldItem(name = "Address", placeHolder = "Address", addressValue, onValueChanged = { currentContact.address = it })
-                textFieldItem(name = "Zip", placeHolder = "Zip", zipValue, onValueChanged = { currentContact.zip = it })
-                textFieldItem(name = "City", placeHolder = "City", cityValue, onValueChanged = { currentContact.city = it })
-                RadioGroup(
-                    items = listOf("Home", "Mobile", "Office", "Fax"),
-                    selected = selected,
-                    setSelected = {
-                        setSelected
-                        when (it) {
-                            "Home" -> currentContact.type = PhoneType.HOME
-                            "Mobile" -> currentContact.type = PhoneType.MOBILE
-                            "Office" -> currentContact.type = PhoneType.OFFICE
-                            "Fax" -> currentContact.type = PhoneType.FAX
-                        }
-                                  },
-                    title = "Phone type"
-                )
-                textFieldItem(name = "Phone number", placeHolder = "Phone number", null, onValueChanged = { currentContact.phoneNumber = it })
-
-                Row(horizontalArrangement = Arrangement.SpaceBetween, modifier = Modifier.fillMaxWidth()) {
-                    Button(onClick = { /*TODO*/ }) {
-                        Text(text = "Cancel")
+            Text(text = "New contact")
+            textFieldItem(name = "Name", placeHolder = "Name", value = currentContact.name, onValueChanged = {
+                setCurrentContact(currentContact.copy(name = it!!))
+            })
+            textFieldItem(name = "Firstname", placeHolder = "Firstname", value = currentContact.firstname, onValueChanged = { setCurrentContact(currentContact.copy(firstname = it!!)) })
+            textFieldItem(name = "Email", placeHolder = "Email", currentContact.email, onValueChanged = { setCurrentContact(currentContact.copy(email = it!!)) })
+            textFieldItem(name = "Birthday", placeHolder = "Birthday", value = simpleDateFormat.format(currentContact.birthday?.time)) //TODO : add date picker? not obligatory
+            textFieldItem(name = "Address", placeHolder = "Address", currentContact.address, onValueChanged = { setCurrentContact(currentContact.copy(address = it!!)) })
+            textFieldItem(name = "Zip", placeHolder = "Zip", currentContact.zip, onValueChanged = { setCurrentContact(currentContact.copy(zip = it!!)) })
+            textFieldItem(name = "City", placeHolder = "City", currentContact.city, onValueChanged = { setCurrentContact(currentContact.copy(city = it!!)) })
+            RadioGroup(
+                items = listOf("Home", "Mobile", "Office", "Fax"),
+                selected = selected,
+                setSelected = {
+                    setSelected(it)
+                    Log.d("RadioGroup", "Selected: $it")
+                    when (it) {
+                        "Home" -> setCurrentContact(currentContact.copy(type = PhoneType.HOME))
+                        "Mobile" -> setCurrentContact(currentContact.copy(type = PhoneType.MOBILE))
+                        "Office" -> setCurrentContact(currentContact.copy(type = PhoneType.OFFICE))
+                        "Fax" -> setCurrentContact(currentContact.copy(type = PhoneType.FAX))
                     }
+                              },
+                title = "Phone type"
+            )
+            textFieldItem(name = "Phone number", placeHolder = "Phone number", currentContact.phoneNumber, onValueChanged = { setCurrentContact(currentContact.copy(phoneNumber = it!!)) })
 
-                    Button(onClick = { /*TODO*/ }) {
-                        Icon(Icons.Outlined.Delete, contentDescription = "Delete")
-                        Text(text = "Delete")
-                    }
+            Row(horizontalArrangement = Arrangement.SpaceBetween, modifier = Modifier.fillMaxWidth()) {
+                Button(onClick = { /*TODO*/ }) {
+                    Text(text = "Cancel")
+                }
 
-                    Button(onClick = {
-                        Log.d("Contact", currentContact.toString())
-                        if (contact == null) {
-                            contactsViewModel.addContact(currentContact)
-                        } else {
-                            contactsViewModel.changeContact(currentContact)
-                        }
-                    }) {
-                        Icon(Icons.Outlined.Create, contentDescription = "Save")
-                        Text(text = "Save")
+                Button(onClick = { /*TODO*/ }) {
+                    Icon(Icons.Outlined.Delete, contentDescription = "Delete")
+                    Text(text = "Delete")
+                }
+
+                Button(onClick = {
+                    Log.d("Contact", currentContact.toString())
+                    if (contact == null) {
+                        contactsViewModel.addContact(currentContact)
+                    } else {
+                        contactsViewModel.changeContact(currentContact)
                     }
+                }) {
+                    Icon(Icons.Outlined.Create, contentDescription = "Save")
+                    Text(text = "Save")
                 }
             }
-
         }
+
+    }
 }
 
 @Composable
